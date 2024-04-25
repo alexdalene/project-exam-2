bede26d0189e9e7732cc351e4f77638c63e735f4
Alex Dalene <aledal51469@stud.noroff.no>
2024-04-25 12:59:45 +0200

    Try Cypress pass test

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,5 +1,10 @@
 describe('visit homepage', () => {
   it('passes', () => {
     cy.visit('/');
+    cy.get('h1').should('text', 'Hello, world!');
+  });
+  it('fails', () => {
+    cy.visit('/');
+    cy.get('h1').should('text', 'Hello, world');
   });
 });
