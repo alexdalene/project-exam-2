511f66d9aef7bf177f4cb18ddfb68b4565d8402e
Alex Dalene <aledal51469@stud.noroff.no>
2024-04-25 08:41:10 +0200

    Adds routes, folder structure and imports custom font

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
 import App from './routes/App';
+import Root from './routes/root';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(
   document.getElementById('root')!,
 ).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
