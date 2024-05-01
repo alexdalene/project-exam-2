3c9dae1e53559d4eb5bbcf7afdbae917197d9898
Alex Dalene <aledal51469@stud.noroff.no>
2024-05-01 15:48:44 +0200

    Add loader on entering app

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,9 +1,29 @@
 import { Button } from '@/components/ui/button';
 import { ArrowRight } from 'lucide-react';
 
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+import { useRef } from 'react';
+
 const Hero = () => {
+  gsap.registerPlugin(useGSAP);
+
+  const sectionRef = useRef(null!);
+
+  useGSAP(() => {
+    gsap.from('h1', {
+      opacity: 0,
+      y: 100,
+      duration: 1,
+    });
+  });
+
   return (
-    <section className="flex min-h-[100svh] w-full flex-col justify-between px-4 py-16">
+    <section
+      className="flex min-h-[100svh] w-full flex-col justify-between px-4 py-16"
+      ref={sectionRef}
+    >
       <h1 className=" text-5xl font-semibold text-muted">
         We'll help you{' '}
         <span className="text-foreground">
