e4df13cd3134b60b9ede964f54a499b248956f57
Alex Dalene <aledal51469@stud.noroff.no>
2024-05-02 18:50:04 +0200

    Add conditional rendering

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,15 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import EarthContainer from '@/components/earth/earth-container';
 import Loading from '@/components/loading';
+import { useLoadingStore } from '@/store/loading';
 
 const Root = () => {
+  const isLoading = useLoadingStore(
+    (state) => state.isLoading,
+  );
+
   return (
     <>
       <div className="fixed left-0 top-0 z-0 h-full w-full overflow-hidden">
         <EarthContainer />
       </div>
       <main className="z-10">
-        <Loading />
+        {isLoading && <Loading />}
         <Outlet />
       </main>
     </>
