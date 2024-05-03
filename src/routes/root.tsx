44987f1f836d50ee0bf6732f8f63642727f7a742
Alex Dalene <aledal51469@stud.noroff.no>
2024-05-03 08:52:03 +0200

    Add navigation component and conditional rendering

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,9 @@
 import EarthContainer from '@/components/earth/earth-container';
 import Loading from '@/components/loading';
+import Navbar from '@/components/navbar';
+
 import { useLoadingStore } from '@/store/loading';
+import { useAnimationStore } from '@/store/animation';
 
 import { Outlet } from 'react-router-dom';
 
@@ -9,11 +12,18 @@ const Root = () => {
     (state) => state.isLoading,
   );
 
+  const isFinished = useAnimationStore(
+    (state) => state.isFinished,
+  );
+
   return (
     <>
       <div className="fixed left-0 top-0 z-0 h-full w-full overflow-hidden">
         <EarthContainer />
       </div>
+
+      {isFinished && <Navbar />}
+
       <main className="z-10">
         {isLoading && <Loading />}
         <Outlet />
