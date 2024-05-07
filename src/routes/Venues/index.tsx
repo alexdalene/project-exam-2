afd0a56369217b080ed82976805283817ec767c9
Alex Dalene <aledal51469@stud.noroff.no>
2024-05-07 14:46:48 +0200

    Clean component for edit

--- src/routes/Venues/index.tsx
+++ src/routes/Venues/index.tsx
@@ -1,21 +1,11 @@
-import { useVenueStore } from '@/store/venues';
 import { useLoadingStore } from '@/store/loading';
 import { useTimelineStore } from '@/store/timeline';
 
 import { useEffect } from 'react';
 
-import Venue from '@/components/venues/Venue';
-
 const Venues = () => {
-  const fetchVenues = useVenueStore((state) => state.fetchVenues);
-  const venues = useVenueStore((state) => state.venues);
-
   const isLoading = useLoadingStore((state) => state.isLoading);
   const updateAct = useTimelineStore((state) => state.updateAct);
-
-  useEffect(() => {
-    fetchVenues();
-  }, [fetchVenues]);
 
   useEffect(() => {
     if (!isLoading) {
@@ -24,11 +14,9 @@ const Venues = () => {
   }, [isLoading, updateAct]);
 
   return (
-    <div className="grid min-h-[inherit] gap-16 px-4 py-3">
-      {venues.map((venue) => {
-        return <Venue key={venue.id} {...venue} />;
-      })}
-    </div>
+    <section>
+      <h1>Venues</h1>
+    </section>
   );
 };
 
