b588b7c092b3b70ccfdb68a3b10e6a18b34fbea0
Alex Dalene <aledal51469@stud.noroff.no>
2024-05-16 17:27:36 +0200

    Remove filters and transition to states instead of react-router-dom loaders

--- src/routes/venues/all.tsx
+++ src/routes/venues/all.tsx
@@ -1,112 +1,33 @@
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from '@/components/ui/pagination';
+import useStore from '@/store/venueStore';
+
 import Venue from '@/components/Venue';
+import PaginationComponent from '@/components/Pagination';
 
-import type { VenueType } from '@/types/venue';
-import type { MetaType } from '@/types/response';
-
-import { useLoaderData, useRouteLoaderData } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
 const VenuesAll = () => {
-  const { venues, meta } = useLoaderData() as {
-    venues: VenueType[];
-    meta: MetaType;
-  };
-
-  const [filteredVenues, setFilteredVenues] = useState<VenueType[]>(venues);
-
-  const { filters } = useRouteLoaderData('venues') as {
-    filters: { price: string; amenities: string; guests: string };
-  };
+  const { venues, meta, loading, error, fetchVenues } = useStore();
 
   useEffect(() => {
-    if (filters) {
-      const filtered = venues.filter((venue) => {
-        if (filters.price) {
-          if (venue.price > parseInt(filters.price)) {
-            return false;
-          }
-        }
-
-        if (filters.amenities) {
-          const amenities = filters.amenities.split(',');
-
-          for (const amenity of amenities) {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            if (!venue.meta[amenity]) {
-              return false;
-            }
-          }
-        }
-
-        if (filters.guests) {
-          if (venue.maxGuests < parseInt(filters.guests)) {
-            return false;
-          }
-        }
-
-        return true;
-      });
-
-      setFilteredVenues(filtered);
-    }
-  }, [filters, venues]);
-
-  console.log(filteredVenues);
+    fetchVenues();
+  }, [fetchVenues]);
 
   return (
     <>
-      <div className="grid grid-cols-1 pt-8 md:grid-cols-4">
-        {!filteredVenues
-          ? venues.map((venue) => <Venue key={venue.id} {...venue} />)
-          : filteredVenues.map((venue) => <Venue key={venue.id} {...venue} />)}
-      </div>
+      {error && <div>Error: {error}</div>}
+      {!error && (
+        <div
+          className="grid grid-cols-1 gap-x-4 gap-y-8 px-4 pt-8 sm:grid-cols-2 md:grid-cols-3 md:px-8 lg:grid-cols-4 lg:px-16"
+          aria-busy={loading}
+          aria-live="polite"
+        >
+          {venues.map((venue) => (
+            <Venue key={venue.id} {...venue} />
+          ))}
+        </div>
+      )}
 
-      <Pagination>
-        <PaginationContent>
-          {!meta.isFirstPage && (
-            <>
-              <PaginationItem>
-                <PaginationPrevious to={`?page=${meta.previousPage}`} />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink to={`?page=${meta.previousPage}`}>
-                  {meta.previousPage}
-                </PaginationLink>
-              </PaginationItem>
-            </>
-          )}
-          <PaginationItem>
-            <PaginationLink to={`?page=${meta.currentPage}`} isActive>
-              {meta.currentPage}
-            </PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink to={`?page=${meta.nextPage}`}>
-              {meta.nextPage}
-            </PaginationLink>
-          </PaginationItem>
-          {!meta.isLastPage && (
-            <>
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationNext to={`?page=${meta.nextPage}`} />
-              </PaginationItem>
-            </>
-          )}
-        </PaginationContent>
-      </Pagination>
+      {meta && <PaginationComponent meta={meta} />}
     </>
   );
 };
