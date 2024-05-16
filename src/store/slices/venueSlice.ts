e2fdb281dbde2440a7e96a2fb454ff3bbb880363
Alex Dalene <aledal51469@stud.noroff.no>
2024-05-16 20:29:31 +0200

    Add slice and filter actions

--- src/store/slices/venueSlice.ts
+++ src/store/slices/venueSlice.ts
@@ -1,20 +1,29 @@
+import { FilterCriteria } from '@/types/filter';
 import { MetaType } from '@/types/response';
 import { VenueType } from '@/types/venue';
 import { StateCreator } from 'zustand';
 
 export type VenueSlice = {
   venues: VenueType[];
+  filteredVenues: VenueType[];
+  filterCriteria: FilterCriteria | null;
   meta: MetaType;
   loading: boolean;
   error: string | null;
+  filtered: boolean;
   fetchVenues: () => Promise<void>;
+  setFilterCriteria: (criteria: FilterCriteria) => void;
+  applyFilters: () => void;
 };
 
-export const createVenueSlice: StateCreator<VenueSlice> = (set) => ({
+export const createVenueSlice: StateCreator<VenueSlice> = (set, get) => ({
   venues: [],
+  filteredVenues: [],
+  filterCriteria: null,
   meta: {} as MetaType,
   loading: false,
   error: null,
+  filtered: false,
 
   fetchVenues: async () => {
     set({ loading: true, error: null });
@@ -30,9 +39,43 @@ export const createVenueSlice: StateCreator<VenueSlice> = (set) => ({
       );
 
       const data = await response.json();
-      set({ venues: data.data, meta: data.meta, loading: false });
+      set({
+        venues: data.data,
+        meta: data.meta,
+        loading: false,
+      });
     } catch (error) {
       set({ error: (error as Error).message, loading: false });
     }
+  },
+
+  setFilterCriteria: (criteria) => {
+    set({ filterCriteria: criteria });
+  },
+
+  applyFilters: () => {
+    const { venues, filterCriteria } = get();
+
+    if (!filterCriteria) {
+      set({ filtered: false });
+      set({ filteredVenues: [] });
+      return;
+    }
+
+    const filter = venues.filter((venue) => {
+      const matchesPrice =
+        venue.price >= filterCriteria.price[0] &&
+        venue.price <= filterCriteria.price[1];
+      const matchesAmenities = filterCriteria.amenities.every(
+        (amenity) => venue.meta[amenity],
+      );
+      const matchesGuests = venue.maxGuests <= filterCriteria.guests;
+
+      console.log(matchesGuests);
+
+      return matchesPrice && matchesAmenities && matchesGuests;
+    });
+
+    set({ filteredVenues: filter, filtered: true });
   },
 });
