a92df2a1a852718041b767edae7af795fc575841
Alex Dalene <aledal51469@stud.noroff.no>
2024-05-28 21:43:35 +0200

    Disable footer temporarily

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="relative mx-auto grid w-full max-w-[1400px] place-content-center overflow-hidden rounded-t-[6rem] border-t border-border pb-32 pt-16">
+    <footer className="relative mx-auto mt-16 grid w-full max-w-[1400px] place-content-center overflow-hidden rounded-t-[6rem] border-t border-border pb-32 pt-16">
       <p className="text-sm">&copy; Holidaze 2024</p>
     </footer>
   );
