82f607eb75a73b9e36fa83587e7779abf4cf689c
Alex Dalene <aledal51469@stud.noroff.no>
2024-05-28 21:44:55 +0200

    Update styling

--- src/components/profile/ProfileTabs.tsx
+++ src/components/profile/ProfileTabs.tsx
@@ -2,45 +2,104 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import { format } from 'date-fns';
+
+import useStore from '@/store/venueStore';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const ProfileTabs = () => {
+  const { profile, profileLoading } = useStore();
+
   return (
-    <div className="mt-16 max-w-[468px] lg:mt-0">
-      <h2 className="text-sm font-medium text-muted-foreground">Manage</h2>
-      <div className="mt-4">
-        <Tabs defaultValue="venues" className="w-[400px]">
+    <div className="mt-16 w-full max-w-[468px] flex-1 lg:mt-0">
+      <div>
+        <Tabs defaultValue="venues">
           <TabsList>
             <TabsTrigger value="venues">Venues</TabsTrigger>
             <TabsTrigger value="bookings">Bookings</TabsTrigger>
           </TabsList>
           <TabsContent value="venues">
-            <Table>
-              <TableCaption>A list of your venues.</TableCaption>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[100px]">Invoice</TableHead>
-                  <TableHead>Status</TableHead>
-                  <TableHead>Method</TableHead>
-                  <TableHead className="text-right">Amount</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow>
-                  <TableCell className="font-medium">INV001</TableCell>
-                  <TableCell>Paid</TableCell>
-                  <TableCell>Credit Card</TableCell>
-                  <TableCell className="text-right">$250.00</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
+            {!profileLoading && (profile?.venues?.length ?? 0) > 0 ? (
+              <ScrollArea className="h-fit max-h-[300px] w-full rounded-xl border">
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="w-[100px]">ID</TableHead>
+                      <TableHead>Name</TableHead>
+                      <TableHead>Created</TableHead>
+                      <TableHead className="text-right">Price</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {profile?.venues?.map((venue, index) => (
+                      <TableRow key={index} role="button">
+                        <TableCell className="font-medium">
+                          VEN0{index}
+                        </TableCell>
+                        <TableCell className="max-w-[100px] truncate">
+                          {venue.name}
+                        </TableCell>
+                        <TableCell>
+                          {format(new Date(venue.created), 'dd/MM/yyyy')}
+                        </TableCell>
+                        <TableCell className="text-right">
+                          {venue.price},-
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </ScrollArea>
+            ) : (
+              <div className="grid h-[100px] w-full place-content-center rounded-xl border">
+                <p className="text-sm text-muted-foreground">
+                  No venues found.
+                </p>
+              </div>
+            )}
           </TabsContent>
-          <TabsContent value="bookings">Change your password here.</TabsContent>
+          <TabsContent value="bookings">
+            {!profileLoading && (profile?.bookings?.length ?? 0) > 0 ? (
+              <ScrollArea className="h-fit max-h-[300px] w-full rounded-xl border">
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="w-[100px]">ID</TableHead>
+                      <TableHead>Date</TableHead>
+                      <TableHead className="text-right">Price</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {profile?.bookings?.map((booking, index) => (
+                      <TableRow key={index} role="button">
+                        <TableCell className="font-medium">
+                          BOK0{index}
+                        </TableCell>
+                        <TableCell>
+                          {format(new Date(booking.dateFrom), 'LLL dd')} -{' '}
+                          {format(new Date(booking.dateTo), 'LLL dd, y')}
+                        </TableCell>
+                        <TableCell className="text-right">
+                          {booking.venue.price} NOK
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </ScrollArea>
+            ) : (
+              <div className="grid h-[100px] w-full place-content-center rounded-xl border">
+                <p className="text-sm text-muted-foreground">
+                  No bookings found.
+                </p>
+              </div>
+            )}
+          </TabsContent>
         </Tabs>
       </div>
     </div>
