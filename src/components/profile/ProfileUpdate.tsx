1c4034f5218135a9a2029f4a6f1a9ce8742b5b13
Alex Dalene <aledal51469@stud.noroff.no>
2024-05-28 21:45:16 +0200

    Add responsive with drawer / dialog

--- src/components/profile/ProfileUpdate.tsx
+++ src/components/profile/ProfileUpdate.tsx
@@ -9,26 +9,63 @@ import {
 import { Button } from '@/components/ui/button';
 import { Edit2Icon } from 'lucide-react';
 import ProfileForm from './ProfileForm';
+import { useMediaQuery } from 'react-responsive';
+import { useState } from 'react';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
 
 const ProfileUpdate = () => {
+  const isDesktop = useMediaQuery({ query: '(min-width: 768px)' });
+  const [open, setOpen] = useState(false);
+
+  if (isDesktop) {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="secondary" size="icon" className="rounded-xl">
+            <Edit2Icon size={20} />
+            <span className="sr-only">Settings</span>
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Edit your profile settings and information.
+            </DialogDescription>
+          </DialogHeader>
+          <ProfileForm />
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
         <Button variant="secondary" size="icon" className="rounded-xl">
           <Edit2Icon size={20} />
           <span className="sr-only">Settings</span>
         </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>Edit profile</DrawerTitle>
+          <DrawerDescription>
             Edit your profile settings and information.
-          </DialogDescription>
-        </DialogHeader>
-        <ProfileForm />
-      </DialogContent>
-    </Dialog>
+          </DrawerDescription>
+        </DrawerHeader>
+        <div className="px-4">
+          <ProfileForm />
+        </div>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
